ddf21717a0032c889d671123c5c068b9f272537e
Nataliya2401 <102024240+Nataliya2401@users.noreply.github.com>
2022-09-18 21:25:46 +0300

    Create package

---
+++ clean_folder/README.md
@@ -0,0 +1,5 @@
+Clean folder
+This package:
+Sort files in folder, which user input;
+Rename files and folders if its name include cyrylic symbols and ":.," and oter "not correct" symbols
+Result: the list of known extensions, which module sorted and the list of unknown extensions
---
+++ clean_folder/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='sort.py',
+    version='1',
+    description='Sortinf files, return exts',
+    url='https://github.com/Nataliya2401/Python_8_core/blob/main/sort.py',
+    author='Nataliya Feshchenko',
+    author_email='sabotab2000@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    install_requires=['markdown'],
+    entry_points={'console_scripts': [
+        'clean-folder= clean_folder.sort:maim']}
+)
