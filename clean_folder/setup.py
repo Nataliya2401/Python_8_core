39226685d75ccbcf75aad6c1b878974a8bd9f392
Nataliya2401 <102024240+Nataliya2401@users.noreply.github.com>
2022-09-18 22:39:43 +0300

    Edit serup

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_namespace_packages
 
 
 setup(
-    name='sort.py',
+    name='clean.py',
     version='1',
     description='Sorting files, return exts',
-    url='https://github.com/Nataliya2401/Python_8_core/blob/main/sort.py',
+    url='https://github.com/Nataliya2401/Python_8_core/blob/main/clean.py',
     author='Nataliya Feshchenko',
     author_email='sabotab2000@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': [
-        'clean-folder= clean_folder.sort:main']}
+        'clean-folder= clean_folder.clean:main']}
 )
