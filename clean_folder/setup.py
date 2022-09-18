63e443deb607b3633bff365ae3db2833a5b5bdcb
Nataliya2401 <102024240+Nataliya2401@users.noreply.github.com>
2022-09-18 22:13:43 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='sort.py',
     version='1',
-    description='Sortinf files, return exts',
+    description='Sorting files, return exts',
     url='https://github.com/Nataliya2401/Python_8_core/blob/main/sort.py',
     author='Nataliya Feshchenko',
     author_email='sabotab2000@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    install_requires=['markdown'],
     entry_points={'console_scripts': [
         'clean-folder= clean_folder.sort:main']}
 )
